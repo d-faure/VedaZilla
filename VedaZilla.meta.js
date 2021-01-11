fc28c726f27968262e332de4dd338a2be77027ff
d-faure <d-faure@users.noreply.github.com>
2021-01-11 13:23:41 +0100

    Update VedaZilla.meta.js

--- VedaZilla.meta.js
+++ VedaZilla.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        VedaZilla
 // @namespace   https://github.com/d-faure/VedaZilla/
-// @version     0.20
+// @version     0.21
 // @description Veda guild's quick'n'dirty (Violent|Tamper)Monkey userscript for MountyHall
 // @author      disciple
 // @copyright   2019+
